2c0bfe907dba761e2615a08200f65f2f463770a2
Bruno N <brunonaponiello@gmail.com>
2017-10-11 14:03:28 -0300

    Initial commit

---
+++ src/main/java/br/com/bnms/dojocleancode/dojocleancode/DojoCleanCodeApplication.java
@@ -0,0 +1,12 @@
+package br.com.bnms.dojocleancode.dojocleancode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DojoCleanCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DojoCleanCodeApplication.class, args);
+	}
+}
